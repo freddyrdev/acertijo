d319d3c5fa64077eb86b5d85ad2e60f9edba9ee6
Freddy <freddychiriano@gmail.com>
2025-12-22 22:30:07 -0500

    Creacion del archivo en Python

---
+++ acertijo.py
@@ -0,0 +1,26 @@
+# Hola keyla como estas te quiero presentar un mini programa
+# que tendras que decifrar, no es complicado simplemente analiza
+
+# ¿Aun sigues pensando? esta es la ultima parte del acertijo mini
+# para que puedas reclamar tu recompensa :D
+
+# El valor de daniel se asigna a personajeUno
+personajeUno = "Daniel"
+
+print(f"{ personajeUno } encontro una caja con una K grabada :O")
+print(f"Dentro habia una carta grabada de decisiones y curiosidad")
+print(f"Sonrio, pensando que incluso los detalles mas dificiles")
+print(f"Algo fascinante")
+
+print("¡Pero oh problema! en la caja se encontro algo misterioso")
+print("tenia que descifrar lo qUe estaba dentro de la caja si lo")
+print("lograba podria conseguir la palaBra magica para finalizar")
+print("este cuento")
+
+print("¿Cuanto vale el valor decimal 72 en el codigo ASCII?")
+
+# Si encuentras las 4 palabras escondidas en este cuento
+# descubriras 4 letras
+
+# X ocupado, _ desocupado
+# XXXX, ____ kubh
